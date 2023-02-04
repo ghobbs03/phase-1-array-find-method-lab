4b2ebfb8b9b17478e1b63ab110103a7e74078510
Gail Hobbs <gbhobbs97@gmail.com>
2023-02-04 14:36:07 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(array) {
+    const obj = array.find(element => element.result === "W");
+
+    if (obj === undefined) {
+        return undefined;
+    }
+    
+    return obj.year;
+}
+
